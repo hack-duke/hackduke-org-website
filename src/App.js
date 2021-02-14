947b9760df6d8c792bc44ba30bd174fa1e7c64a7
James Leong <jameszleong@gmail.com>
2021-02-13 23:32:14 -0500

    add animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 import About from "./pages/About";
 import Humans from "./pages/Humans";
 import Events from "./pages/Events";
@@ -11,27 +15,33 @@ export default function App() {
   return (
       //TODO: create navigation bar
       <Router>
-          <div className={"main-app"}>
-            {/*switching between routes*/}
-            <Switch>
-              <Route exact path="/about">
-                <About />
-              </Route>
-              <Route exact path="/humans">
-                <Humans />
-              </Route>
-              <Route exact path="/events">
-                <Events />
-              </Route>
-              <Route path="/join">
-                <Recruitment />
-              </Route>
-              <Route path="/">
-                <Home/>
-              </Route>
-            </Switch>
-
-          </div>
+        <Route render={({location}) => (
+            <TransitionGroup className={"main-app"}>
+              <CSSTransition
+                  key={location.key}
+                  timeout={300}
+                  classNames="fade"
+              >
+                <Switch location={location}>
+                  <Route exact path="/about">
+                    <About />
+                  </Route>
+                  <Route exact path="/humans">
+                    <Humans />
+                  </Route>
+                  <Route exact path="/events">
+                    <Events />
+                  </Route>
+                  <Route path="/join">
+                    <Recruitment />
+                  </Route>
+                  <Route path="/">
+                    <Home/>
+                  </Route>
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+        )} />
       </Router>
   );
 }
