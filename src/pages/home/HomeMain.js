a2d0db4056ec69ef85d11ed4625505a7a00d184a
James Leong <jameszleong@gmail.com>
2021-02-14 23:00:30 -0500

    change bg images and height

--- src/pages/home/HomeMain.js
+++ src/pages/home/HomeMain.js
@@ -4,8 +4,10 @@ import "./HomeMain.css";
 
 function HomeMain() {
     return (
-        <div className={"home-main"}>
-            <img className={"hd-logo"} src={hdLogo} alt={"hd-logo"}/>
+        <div className={"home-bg"}>
+            <div className={"home-main"}>
+                <img className={"hd-logo"} src={hdLogo} alt={"hd-logo"}/>
+            </div>
         </div>
     )
 }
