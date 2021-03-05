7318d723576f2314fefc56649fa3b8f72df01b58
James Leong <jameszleong@gmail.com>
2021-03-05 16:46:42 -0500

    text changes

--- src/pages/recruitment/RecruitmentBody.js
+++ src/pages/recruitment/RecruitmentBody.js
@@ -7,7 +7,7 @@ function RecruitmentBody() {
             <div className={"recruit-text-body"}>
                 <h2>It's a simple, 2-step process.</h2>
                 <h3>1. Written Application</h3>
-                <p>The application form will be released soon! It will a few short answer questions.</p>
+                <p>The application form will be released soon! It will contain a few short answer questions.</p>
                 <h3>2. 30-minute interview</h3>
                 <p>Sign up for an interview with a HackDuke member so we can get a better understanding of your interest and background! It’ll be pretty casual and conversational but still come prepared. We want to see your passion!</p>
             </div>
