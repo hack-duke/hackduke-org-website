05b68d36b571d482397502c24b3e850e18f9a7d7
James Leong <jameszleong@gmail.com>
2021-03-05 18:09:11 -0500

    update hackduke link

--- src/pages/events/CodeForGood.js
+++ src/pages/events/CodeForGood.js
@@ -20,7 +20,7 @@ function CodeForGood() {
     return (
         <div id="codeforgood" className={"code-for-good"}>
             <div className={"hd-text"}>
-                <h3 style={{fontWeight: "600"}}>Code For Good <a href={"https://hackduke.org/"} rel="noreferrer" target="_blank"><img src={hyperlink}/></a></h3>
+                <h3 style={{fontWeight: "600"}}>Code For Good <a href={"https://2020.hackduke.org/"} rel="noreferrer" target="_blank"><img src={hyperlink}/></a></h3>
                 <p>Code for Good is the nation’s premier hackathon for tech and social good. We unite students that are passionate about changing the intention and narrative behind how technology is used today to create meaningful hacks in a 24-hour sprint. Our goal is to facilitate an environment where students can create meaningful technical projects or learn the skills to get there.</p>
                 <a href={"https://www.youtube.com/watch?v=UGSj5e3Jglg&list=PLtFr9wE3pO0SbreqpM3arNj4OY7FzAXMq"} rel="noreferrer" target="_blank" className={"speakers-button"} style={{color: "white"}}>Watch our 2020 speakers <i className="fas fa-arrow-right"/></a>
             </div>
