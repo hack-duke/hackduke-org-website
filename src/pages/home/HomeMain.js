9fce15f8b71ea46da92d5ba68aa055a1499e77df
Alan Bi <alan.bi326@gmail.com>
2021-08-06 20:04:59 -0700

    Fix styling and text

--- src/pages/home/HomeMain.js
+++ src/pages/home/HomeMain.js
@@ -7,7 +7,7 @@ function HomeMain() {
         <div id="main" className={"home-bg"}>
             <div className={"home-main"}>
                 <img className={"hd-logo"} src={hdLogo} alt={"hd-logo"}/>
-                <a href="https://2021.hackduke.org">
+                <a href="https://2021.hackduke.org" className={"home-main-button"} target="_blank" rel="noreferrer">
                     <span className={"home-main-button"}>
                         Enter 2021 site&nbsp;
                         <i className="fas fa-arrow-right"/>
