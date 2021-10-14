c7f21a1007216d3ce44a38b1debe8f7595ac18de
Alex Desbans <ard56@duke.edu>
2021-10-14 12:10:20 -0400

    Tech Team Info Updated

--- src/pages/humans/Tech.js
+++ src/pages/humans/Tech.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import james from "../../graphics/team-photos/james.jpeg";
-import braden from "../../graphics/team-photos/braden.png";
-import frankie from "../../graphics/team-photos/frankie.jpeg";
 import alex from "../../graphics/team-photos/alex.jpeg";
-import han from "../../graphics/team-photos/hanzhang.jpg";
+import jacob from "../../graphics/team-photos/jacob.jpeg";
+import johnny from "../../graphics/team-photos/johnny.jpeg";
+import ruohan from "../../graphics/team-photos/ruohan.jpg";
+import khushmeet from "../../graphics/team-photos/khushmeet.jpg";
+import dennis from "../../graphics/team-photos/dennis.png";
 
 function Tech() {
     return (
@@ -12,31 +13,36 @@ function Tech() {
             <p style={{fontSize: "20px", marginBottom: "30px"}}>Magicians, Sorcerers, Wizards. Coding is like magic, no doubt, no doubt, no doubt.</p>
             <div className={"profile-grid"}>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={james} />
-                    <h3 className={"profile-name"}>James Leong</h3>
+                    <img alt="" className={"profile-headshot"} src={alex} />
+                    <h3 className={"profile-name"}>Alex Desbans</h3>
+                    <p className={"profile-title"}>Co-Lead</p>
+                    <p className={"profile-major"}>ECE & Computer Science</p>
+                </div>
+                <div className={"profile-container"}>
+                    <img alt="" className={"profile-headshot"} src={dennis} />
+                    <h3 className={"profile-name"}>Dennis Xu</h3>
                     <p className={"profile-title"}>Co-Lead</p>
                     <p className={"profile-major"}>Computer Science</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={braden} />
-                    <h3 className={"profile-name"}>Braden Hoagland</h3>
-                    <p className={"profile-title"}>Co-Lead</p>
-                    <p className={"profile-major"}>Mathematics</p>
+                    <img alt="" className={"profile-headshot"} src={jacob} />
+                    <h3 className={"profile-name"}>Jacob Liu</h3>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={frankie} />
-                    <h3 className={"profile-name"}>Frankie Willard</h3>
-                    <p className={"profile-major"}>Computer Science & Mathematics</p>
+                    <img alt="" className={"profile-headshot"} src={johnny} />
+                    <h3 className={"profile-name"}>Johnny Pesavento</h3>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={alex} />
-                    <h3 className={"profile-name"}>Alex Desbans</h3>
-                    <p className={"profile-major"}>ECE & Computer Science</p>
+                <img alt="" className={"profile-headshot"} src={khushmeet} />
+                    <h3 className={"profile-name"}>Khushmeet Chandi</h3>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={han} />
-                    <h3 className={"profile-name"}>Han Zhang</h3>
-                    <p className={"profile-major"}>Computer Science & Economics</p>
+                <img alt="" className={"profile-headshot"} src={ruohan} />
+                    <h3 className={"profile-name"}>Ruohan Zhang</h3>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
             </div>
         </div>
