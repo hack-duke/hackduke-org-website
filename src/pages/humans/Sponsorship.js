c195141d5ac5a4edcc95ce22613f0e3abdf2edfd
Tony Wu <uutonywu@gmail.com>
2022-01-11 23:09:27 -0800

    Update sponsorship team photos

--- src/pages/humans/Sponsorship.js
+++ src/pages/humans/Sponsorship.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import della from "../../graphics/team-photos/della.jpg";
-import raymond from "../../graphics/team-photos/raymond.jpg";
-import jason from "../../graphics/team-photos/jason.jpeg";
+import christian from "../../graphics/team-photos/christian.jpg";
+import dennis from "../../graphics/team-photos/dennis.jpg";
+import alan from "../../graphics/team-photos/alan.jpeg";
+import islina from "../../graphics/team-photos/islina.jpg";
+import sudhir from "../../graphics/team-photos/sudhir.jpg";
 
 function Sponsorship() {
     return (
@@ -10,21 +12,31 @@ function Sponsorship() {
             <p style={{fontSize: "20px", marginBottom: "30px"}}>Making money moves. Need I say more?</p>
             <div className={"profile-grid"}>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={della} />
-                    <h3 className={"profile-name"}>Della Sihite</h3>
+                    <img alt="" className={"profile-headshot"} src={christian} />
+                    <h3 className={"profile-name"}>Christian Okokhere</h3>
                     <p className={"profile-title"}>Co-Lead</p>
-                    <p className={"profile-major"}>Computer Science & Economics</p>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={raymond} />
-                    <h3 className={"profile-name"}>Raymond Lin</h3>
+                    <img alt="" className={"profile-headshot"} src={dennis} />
+                    <h3 className={"profile-name"}>Dennis Xu</h3>
                     <p className={"profile-title"}>Co-Lead</p>
-                    <p className={"profile-major"}>ECE & Computer Science</p>
+                    <p className={"profile-major"}>Computer Science & Math</p>
                 </div>
                 <div className={"profile-container"}>
-                    <img alt="" className={"profile-headshot"} src={jason} />
-                    <h3 className={"profile-name"}>Jason Yang</h3>
-                    <p className={"profile-major"}>ECE & Computer Science</p>
+                    <img alt="" className={"profile-headshot"} src={alan} />
+                    <h3 className={"profile-name"}>Alan Bi</h3>
+                    <p className={"profile-major"}>Computer Science</p>
+                </div>
+                <div className={"profile-container"}>
+                    <img alt="" className={"profile-headshot"} src={islina} />
+                    <h3 className={"profile-name"}>Islina Shan</h3>
+                    <p className={"profile-major"}>Computer Science</p>
+                </div>
+                <div className={"profile-container"}>
+                    <img alt="" className={"profile-headshot"} src={sudhir} />
+                    <h3 className={"profile-name"}>Sudhir Chowdhry</h3>
+                    <p className={"profile-major"}>Computer Science</p>
                 </div>
             </div>
         </div>
