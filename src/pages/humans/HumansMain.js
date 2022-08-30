dbfc949ada746c1df19a8e06ad3fd0cbdd30d0e4
Graham Gumbert <gjgumbert@gmail.com>
2022-08-30 16:36:10 -0400

    changed team photo

--- src/pages/humans/HumansMain.js
+++ src/pages/humans/HumansMain.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./HumansMain.css";
-import teamPhoto from "../../graphics/team-photo-2021.png"
+import teamPhoto from "../../graphics/team-photo-2022.jpg";
 
 function HumansMain() {
-    return (
-        <div id="main" className={"humans-main"}>
-            <div className={"humans-text"}>
-                <h1>Hey!</h1>
-                <p>We are a group of Duke students who work together to bring to life Code for Good and Ideate. Although we all have different interests and backgrounds, we value our community and are united by our love of building something cool. We are split into 5 teams (Tech, Design, Logistics, Outreach, Sponsorship) and are lead by our two amazing co-directors! </p>
-            </div>
-            <div className={"team-photo"} >
-                <img src={teamPhoto} alt={"team"} style={{width: "500px"}}/>
-            </div>
-        </div>
-    )
+  return (
+    <div id="main" className={"humans-main"}>
+      <div className={"humans-text"}>
+        <h1>Hey!</h1>
+        <p>
+          We are a group of Duke students who work together to bring to life
+          Code for Good and Ideate. Although we all have different interests and
+          backgrounds, we value our community and are united by our love of
+          building something cool. We are split into 5 teams (Tech, Design,
+          Logistics, Outreach, Sponsorship) and are lead by our two amazing
+          co-directors!{" "}
+        </p>
+      </div>
+      <div className={"team-photo"}>
+        <img src={teamPhoto} alt={"team"} style={{ width: "500px" }} />
+      </div>
+    </div>
+  );
 }
 
 export default HumansMain;
