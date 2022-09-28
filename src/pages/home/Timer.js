a6f7833b253451ddfa9086bf8c76fdacbd3c727d
Yihong Song <ys377@duke.edu>
2022-09-28 17:37:29 -0400

    Added Code for Good Button

--- src/pages/home/Timer.js
+++ src/pages/home/Timer.js
@@ -2,57 +2,64 @@ import React, { useState, useEffect } from "react";
 import "./Timer.css";
 
 const Timer = () => {
-  const createTimer = () => {
-    var cfgDate = new Date("10/22/2022");
-    cfgDate.setHours(cfgDate.getHours() + 8);
-    let dif = +cfgDate - +new Date();
-    let timeRemaining = {
-      days: Math.floor(dif / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((dif / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((dif / (1000 * 60)) % 60),
-      seconds: Math.floor((dif / 1000) % 60),
+    const createTimer = () => {
+        var cfgDate = new Date("10/22/2022");
+        cfgDate.setHours(cfgDate.getHours() + 8);
+        let dif = +cfgDate - +new Date();
+        let timeRemaining = {
+            days: Math.floor(dif / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((dif / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((dif / (1000 * 60)) % 60),
+            seconds: Math.floor((dif / 1000) % 60),
+        };
+
+        return timeRemaining;
     };
 
-    return timeRemaining;
-  };
+    const [timeLeft, setTimeLeft] = useState(createTimer());
 
-  const [timeLeft, setTimeLeft] = useState(createTimer());
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(createTimer());
+        }, 1000);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(createTimer());
-    }, 1000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [timeLeft]);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [timeLeft]);
+    const timerComponents = [];
 
-  const timerComponents = [];
+    Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
+        timerComponents.push(
+            <span>
+                {timeLeft[interval]} {interval}{" "}
+            </span>
+        );
+    });
 
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
+    return (
+        <div className="timer">
+            Code for Good 2022 - October 22-23
+            <br />
+            {timerComponents.length ? (
+                timerComponents
+            ) : (
+                <span>0 days 0 hours 0 minutes 0 seconds</span>
+            )}
+            <div
+                onClick={() =>
+                    window.open("https://2022.hackduke.org/", "_blank")
+                }
+                className="join">
+                Apply Here!
+            </div>
+        </div>
     );
-  });
-
-  return (
-    <div className="timer">
-      Code for Good 2022 - October 22-23
-      <br />
-      {timerComponents.length ? (
-        timerComponents
-      ) : (
-        <span>0 days 0 hours 0 minutes 0 seconds</span>
-      )}
-    </div>
-  );
 };
 
 export default Timer;
